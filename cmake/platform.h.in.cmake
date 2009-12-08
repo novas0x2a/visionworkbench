9af34e41ea5b7d53918a7f850e8a1c0cd9119f3a
Mark Allan <mark.b.allan@nasa.gov>
2009-12-08 06:35:52 +0000

    removed some warnings and started on Plate
    git-svn-id: https://babelfish.arc.nasa.gov/svn/visionworkbench/branches/roversw@2370 9cace3f9-8015-0410-999b-c7bcc5930bcc

--- cmake/platform.h.in.cmake
+++ cmake/platform.h.in.cmake
@@ -4,52 +4,76 @@
 /*
  * Some quickie fixes to make life a little simpler under win32...
  */
-#ifdef WIN32
+#ifdef _WIN32
+  #ifndef WIN32_WORKAROUNDS_INCLUDED
+  #define WIN32_WORKAROUNDS_INCLUDED
+
+  // Disable retarded win32 'min' and 'max' macros
+  #ifndef NOMINMAX
+  #  define NOMINMAX
+  #endif
+  
   #define _USE_MATH_DEFINES
   #include <windows.h>
   #include <math.h>
   #include <stdlib.h>
-
-  inline long lround(double val) {
-    return (long)(val > 0 ? floor(val+0.5) : ceil(val-0.5));
-  }
-  inline float nearbyintf(float val) {
-    return (val > 0 ? floorf(val+0.5f) : ceilf(val-0.5f));
-  }
-  inline float roundf(float val) {
-  	return nearbyintf(val);
-  }
-  inline double nearbyint(double val) {
-    return (val > 0 ? floor(val+0.5) : ceil(val-0.5));
-  }
-  inline double round(double val) {
-    return nearbyint(val);
-  }
-  inline double log2(double x) {
-    return log(x)/log(2.0); 
-  }
   
-  inline int random() {
-    return rand();
-  }
-  inline void srandom( unsigned int seed ) {
-    srand(seed);
-  }
-
-  #define snprintf     _snprintf
-  #define vsnprintf    _vsnprintf
-  #define strcasecmp   _stricmp
-  #define strncasecmp  _strnicmp
+  namespace win32_workarounds {
   
-  inline double hypot (double a, double b) { return _hypot(a,b); }
-  inline float  hypotf(float a, float b)   { return _hypotf(a,b); }
-  // XXX FIXME XXX 
-  // XXX there does not appear to be a _hypotl, 
-  // XXX so use _hypot as a poor workaround
-  inline long double hypotl(double a, double b) {
-    return _hypot(a,b);
-  }
+    inline long lround(double val) {
+      return (long)(val > 0 ? floor(val+0.5) : ceil(val-0.5));
+    }
+    inline float nearbyintf(float val) {
+      return (val > 0 ? floorf(val+0.5f) : ceilf(val-0.5f));
+    }
+    inline float roundf(float val) {
+  	  return nearbyintf(val);
+    }
+    inline double nearbyint(double val) {
+      return (val > 0 ? floor(val+0.5) : ceil(val-0.5));
+    }
+    inline double round(double val) {
+      return nearbyint(val);
+    }
+    inline double log2(double x) {
+      return log(x)/log(2.0); 
+    }
 
+    inline int random() {
+      return rand();
+    }
+    inline void srandom( unsigned int seed ) {
+      srand(seed);
+    }
+
+    //inline double hypot (double a, double b) { return _hypot(a,b); }
+    inline float  hypotf(float a, float b)   { return _hypotf(a,b); }
+    // XXX FIXME XXX 
+    // XXX there does not appear to be a _hypotl, 
+    // XXX so use _hypot as a poor workaround
+    inline long double hypotl(double a, double b) {
+      // is there a VC++ equivalent to #warning?
+      return _hypot(a,b);
+    }
+  } // namespace win32_workarounds
+  using namespace win32_workarounds;
+
+  /*
+   * use macros for calls with varargs
+   */
+  #ifndef snprintf
+  #  define snprintf     _snprintf
+  #endif
+  #ifndef vsnprintf
+  #  define vsnprintf    _vsnprintf
+  #endif
+  #ifndef strcasecmp
+  #  define strcasecmp   _stricmp
+  #endif
+  #ifndef strncasecmp
+  #  define strncasecmp  _strnicmp
+  #endif
+  
   /*
    * we shouln't actually need these if 
    * _USE_MATH_DEFINES is defined before 
@@ -65,6 +89,7 @@
   #include <boost/cstdint.hpp>
   using namespace boost;
 
-#endif//WIN32
+  #endif // WIN32_WORKAROUNDS_INCLUDED
+#endif// _WIN32
 
 #endif//__VW__PLATFORM_H__ 
